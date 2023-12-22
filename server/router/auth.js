ebaa78ec273d1bdc0cbf9656c2727dac7e8d637a
Atul Paswan <atulpaswan324@gmail.com>
2023-12-22 15:07:32 +0530

    create login api

--- server/router/auth.js
+++ server/router/auth.js
@@ -6,24 +6,60 @@ const router = express.Router();
 router.get('/',(req, res)=>{
     res.send("THis is from router file");
 });
-router.post('/register',(req, res)=>{
+router.post('/register', async(req, res)=>{
     const {name, email,phone, work, password, cpassword} = req.body;
     if(!name ||!email || !phone || !work || !password || !cpassword )
     {
-        return res.status(422).json({error: "nfasdm,"});
+        return res.status(422).json({error: "Please fill every required field"});
     }
-    User.findOne({email: email})
-    .then((userExists)=>{
+    try {
+
+        const userExists = await User.findOne({email: email})
         if(userExists) {
             return res.status(422).json({error: "Email already exists"});
         }
         const user = new User({name, email,phone, work, password, cpassword});
-        user.save().then(()=>{
-            res.status(201).json({message: "user successfully registered"})
-            .catch((err)=> res.status(500).json({error: "Failed to register user"}))
-        })
-    }).catch((err)=> {console.log(err)})
+        await user.save();
+        res.status(201).json({message: "user registered successfully"});
+
+        
+    } catch (err) {
+        console.log(err);
+    }
+    
+    
    
+});
+
+
+// signin route 
+
+router.post('/signin', async(req, res)=>{
+
+    try {
+        
+        const {email, password} = req.body;
+        if(!email || !password)
+        {
+            return res.status(400).json({err:"Invalid login or password. Please try again."});
+        }
+        const userLogin = await User.findOne({email: email});
+        console.log(userLogin);
+
+        if(!userLogin)
+        {
+            res.status(400).json({err:" Invalid login or password. Please try again."});
+        }
+        else{
+
+            res.status(200).json({message: 'User logged in successfully'});
+        }
+        
+    } catch (error) {
+        
+    }
+
+
 });
 
 module.exports = router;
